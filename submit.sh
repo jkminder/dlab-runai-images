f561a630986d86ca5e5adf04395ad5eac43862a4
Julian Minder <34280944+jkminder@users.noreply.github.com>
2024-05-20 16:17:06 +0200

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -2,7 +2,7 @@
 
 # This script is used to submit a job to the RUNAI platform.
 # Replace {GASPAR_USERNAME} with your username and {IMAGE} with the image you want to run, e.g. ghcr.io/jkminder/dlab-runai-images/base:master
-GASPAR_USERNAME='jminder'
+GASPAR_USERNAME='...'
 IMAGE='ghcr.io/jkminder/dlab-runai-images/base:master'
 
 # Example Usage:
